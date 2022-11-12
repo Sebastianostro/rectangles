d28a3af951547ec264310656280ab2920bd91320
Sebastian Ostrowski <sebastian.ostrowski@hotmail.com>
2022-11-12 13:04:46 +0100

    Implemented method "area" on struct "Rectangle"

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect = Rectangle{
         width: 50,
         height: 30,
     };
     
-    println!("The area of the rectangle {:#?} is {} square pixels.", rect,area(&rect));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle {:#?} is {} square pixels.", rect, rect.area());
 }
