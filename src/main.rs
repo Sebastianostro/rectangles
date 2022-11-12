37f3759ef2f55d9c227ba6fd58de17a6627ecce3
Sebastian Ostrowski <sebastian.ostrowski@hotmail.com>
2022-11-12 13:31:23 +0100

    Implementing another method "can hold"

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,30 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other_rect : &Rectangle) -> bool {
+        self.width > other_rect.width && self.height > other_rect.height
+    }
 }
 
 fn main() {
-    let rect = Rectangle{
-        width: 50,
-        height: 30,
+    let rect1 = Rectangle{
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle{
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle{
+        width: 40,
+        height: 60,
     };
     
-    println!("The area of the rectangle {:#?} is {} square pixels.", rect, rect.area());
+    println!("The area of the rectangle {:#?} is {} square pixels.", rect1, rect1.area());
+    // Check if rect1 can fully cover rect2 and rect3, resp.
+    println!("Can rect1 fully cover rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 fully cover rect3? {}", rect1.can_hold(&rect3));
 }
