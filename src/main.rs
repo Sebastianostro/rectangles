f7efb0db884eb2f59478e74e2b29f0791fdb0867
Sebastian Ostrowski <sebastian.ostrowski@hotmail.com>
2022-11-12 09:44:09 +0100

    Working version using struct type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle{
+        width: 50,
+        height: 30,
+    };
+    
+    println!("The area of the rectangle {:#?} is {} square pixels.", rect,area(&rect));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
